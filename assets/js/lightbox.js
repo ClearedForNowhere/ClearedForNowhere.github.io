3f30a1b846bd351f3db568cbab14ee135b8dbb1e
ClearedForNowhere <pierre.iozzoli@gmail.com>
2026-09-14 23:40:49 +0200

    Update lightbox.js

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -5,7 +5,26 @@ document.addEventListener('DOMContentLoaded', function () {
   images.forEach(function (image) {
 
     image.addEventListener('click', function () {
-      console.log('Image cliquée !');
+
+      // Create the lightbox
+      const lightbox = document.createElement('div');
+      lightbox.classList.add('lightbox');
+
+      // Create the enlarged image
+      const enlargedImage = document.createElement('img');
+      enlargedImage.src = image.src;
+
+      // Add the image to the lightbox
+      lightbox.appendChild(enlargedImage);
+
+      // Add the lightbox to the page
+      document.body.appendChild(lightbox);
+
+      // Close the lightbox when clicking on the background
+      lightbox.addEventListener('click', function () {
+        lightbox.remove();
+      });
+
     });
 
   });
